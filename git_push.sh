e49cf5839da6c305be63af1378d36375e9bd0ae0
StdBay <1224342775@qq.com>
2022-08-26 14:31:03 +0800

    Fri, 26 Aug 2022 14:31:03 +0800

--- git_push.sh
+++ git_push.sh
@@ -3,11 +3,6 @@ RED='\e[31m'
 GREEN='\e[32m'
 END='\e[0m\n'
 
-recent_file_folder=$(cd $(dirname ${BASH_SOURCE[0]}); pwd )
-section=${recent_file_folder##*/}
-git_branch=`git symbolic-ref --short -q HEAD | grep '**'` #具体的开发分支，如requirment170914
-echo -e $GREEN "尝试推送$section的$git_branch分支" $END
-
 ##### here define the commit message #####
 if [ $# -eq 0 ]
 then
@@ -23,11 +18,9 @@ commit_result=`git commit -m "${pushmessage}"`
 
 # 使用双括号防止shell把commit_result理解成多个字符串
 if [[ $commit_result =~ "clean" ]]||[[ $commit_result =~ "干净" ]];then
-    echo -e $GREEN "已与最新的远程仓库同步，不需要提交也不需要推送" $END
+    echo -e $GREEN "无需提交，开始推送到远程仓库..." $END
 elif [[ $commit_result =~ "changed" ]];then
     echo -e $GREEN "提交成功，开始推送到远程仓库..." $END
-elif [[ $commit_result =~ "领先" ]];then
-    echo -e $GREEN "本地仓库领先于远程仓库，不需要提交，开始推送到远程仓库..." $END
 else
     # 异常情况
     echo -e $RED "异常情况，程序退出，具体见commit信息：" $END
